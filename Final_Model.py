e120dc19c1a26591971d487d84426b92190ba518
YoonjibKim <yoonjib.kim@unb.ca>
2023-06-12 19:39:37 -0300

    1.1v

--- Final_Model.py
+++ Final_Model.py
@@ -34,11 +34,11 @@ class Final_Model:
             param_top_dict = top_dict[top_scenario_initial]
             combination_type = param_top_dict[Constant_Parameters.COMBINATION_TYPE]
             temp_path = default_top_path + '/' + combination_type + '/' + Constant_Parameters.F1_SCORE
-            symbol_index_file_path = temp_path + '/' + top_scenario_initial + Constant_Parameters.SYMBOL_INDEX
 
             feature_combination_list = param_top_dict[Constant_Parameters.FEATURE_COMBINATION]
             feature_index_list = feature_combination_list.split(' + ')
 
+            # symbol_index_file_path = temp_path + '/' + top_scenario_initial + Constant_Parameters.SYMBOL_INDEX
             # with open(symbol_index_file_path, 'r') as f:
             #     symbol_index_dict = json.load(f)
             # symbol_name_list = []
